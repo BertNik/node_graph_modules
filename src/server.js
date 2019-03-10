38668535d4e4a8eb1d9bb03110d927f7a2d72a34
Nik B <nikolaus.bertino@gmail.com>
2019-03-10 01:22:06 -0500

    adding server component

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const processArgs = require('./args');
+const port = processArgs.getPort();
+const fi = require('./file_s');
+const p = require('path');
+
+const http = require('http'); 
+const qs = require('querystring');
+const dd_root = processArgs.getRoot();
+
+const server = http.createServer(function (req, res) {
+
+});
+server.on('request',(req,resp)=>{
+    let q = req.url.slice(req.url.indexOf("?")+1);
+    let body = [];
+    req.on('data',(c)=>{
+        body.push(c);
+    }).on('end',()=>{
+        body = Buffer.concat(body).toString();
+        let a = (async function (){
+            let package = qs.parse(q)['package'];
+            let result = await fi.read_(dd_root,package).then((r)=>{
+                resp.write(r);
+                resp.end("\n");
+            }).catch((e)=>{
+                console.log(e.message);
+                resp.write(e.custom_message+"\n");
+                resp.end();
+            });
+        })()
+    })
+});    
+server.listen(port); 
+console.log(`Node.js web server at port ${port} is running..`)

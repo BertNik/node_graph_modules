c9ce27e5b524caf89703db774f65b069f4db8c67
Nik B <nikolaus.bertino@gmail.com>
2019-03-10 01:20:13 -0500

    adding more parameters

--- src/args.js
+++ src/args.js
@@ -1,10 +1,22 @@
-exports.getPackage = function(process){
-    if(process.argv.indexOf('-p') !== -1 && typeof process.argv[process.argv.indexOf('-p')+1] === 'string')
-        return process.argv[process.argv.indexOf('-p')+1];
+const getParam = (param, process)=>{
+    if(process.argv.indexOf(param) !== -1 && typeof process.argv[process.argv.indexOf(param)+1] === 'string')
+        return process.argv[process.argv.indexOf(param)+1];
     else return;
 }
+exports.getPackage = function(process){
+    const param = "-p";
+    return getParam(param, process);
+    
+}
 exports.getPath = function(){
-    if(process.argv.indexOf('--path') !== -1 && typeof process.argv[process.argv.indexOf('--path')+1] === 'string')
-        return process.argv[process.argv.indexOf('--path')+1];
-    else return;
+    const param = "--path";
+    return getParam(param, process);
+}
+exports.getPort = function(){
+    const param = "--port";
+    return getParam(param, process);
+}
+exports.getRoot = function(){
+    const param = "--dd";
+    return getParam(param, process);
 }

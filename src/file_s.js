08a13d701a25d637f8112ed749e8da119126ce67
Nik B <nikolaus.bertino@gmail.com>
2019-03-10 01:20:46 -0500

    read function is a promise

--- src/file_s.js
+++ src/file_s.js
@@ -5,11 +5,18 @@ exports.write = function(path, packageName, api_data){
         console.log("*".repeat(100)+"\nfile written successfully!\nfile:\t"+`${path}/${packageName}.json`+"\n"+"*".repeat(100));
     });
 };
-exports.read = function(...path_package){
-    fs.readFile(`${path_package[0]}/${path_package[1]}.json`, {encoding:'utf-8'}, (err, data)=>{
-        //console.log(data);
-    })
-}
+
+exports.read_ = function(...path_package){
+    return new Promise ((res,rej) => {
+        fs.readFile(`${path_package[0]}/${path_package[1]}.json`, {encoding:'utf-8'}, (err, data)=>{
+            if(err){
+                err.custom_message = 'package does not exist locally'
+                rej(err);
+            }
+            res(data);
+        })
+    })}
+
 exports.exists = function(...path_package){
     if(fs.existsSync(`${path_package[0]}/${path_package[1]}.json`))return console.log(`${path_package[0]}/${path_package[1]}.json`);
     return false;

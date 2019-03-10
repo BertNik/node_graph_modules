2a1dcf9c0b8f88db8764ad01695fed4e581d753a
Nik B <nikolaus.bertino@gmail.com>
2019-03-10 04:15:11 -0400

    pass boolean on end to avoid re-running

--- src/server.js
+++ src/server.js
@@ -7,26 +7,28 @@ const http = require('http');
 const qs = require('querystring');
 const dd_root = processArgs.getRoot();
 
-const server = http.createServer(function (req, res) {
-
-});
-server.on('request',(req,resp)=>{
+const server = http.createServer(function (req, resp) {
     let q = req.url.slice(req.url.indexOf("?")+1);
     let body = [];
     req.on('data',(c)=>{
         body.push(c);
-    }).on('end',()=>{
+    }).on('end',(f)=>{
         body = Buffer.concat(body).toString();
         let package = qs.parse(q)['package'];
         fi.read(dd_root,package).then((r)=>{
+            if(!r)return;
+            resp.setHeader('Content-Type', 'application/json;')
             resp.write(r);
-            resp.end("\n");
+            resp.end(false);
         }).catch((e)=>{
+            resp.setHeader('Content-Type', 'application/json;')
             console.log(e.message);
             resp.write(e.custom_message+"\n");
             resp.end();
         });
     })
-});    
+})
+//server.on('request',(req,resp)=>{
+//});    
 server.listen(port); 
 console.log(`Node.js web server at port ${port} is running..`)
